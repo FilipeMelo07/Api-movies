4e29f8168540ab4edd6e74dbb7ee808e2e2cecd2
FilipeMelo07 <felipenogueira0716@gmail.com>
2025-09-14 11:46:02 -0300

    feature-2: Adiciona rota POST para salvar filmes no SQLite

--- index.js
+++ index.js
@@ -26,7 +26,28 @@ app.get('/api/filmes', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+// NOVA ROTA: Rota POST para inserir um filme no banco de dados
+app.post('/api/filmes', async (req, res) => {
+    const { titulo, ano } = req.body; // Pega os dados do corpo da requisição
 
+    if (!titulo || !ano) {
+        return res.status(400).json({ error: 'Título e ano são obrigatórios' });
+    }
+
+    try {
+        const db = await getDbConnection();
+        // O '?' previne SQL Injection
+        const result = await db.run('INSERT INTO filmes (titulo, ano) VALUES (?, ?)', [titulo, ano]);
+        
+        // Retorna o filme recém-criado
+        const novoFilme = { id: result.lastID, titulo, ano };
+        await db.close();
+        
+        res.status(201).json(novoFilme);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
